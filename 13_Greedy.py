f7a30c5307d2c55bde3d9939375f7349268dfef3
Ashwin Chafale <chafale@usc.edu>
2023-04-08 15:45:05 -0700

    update

--- 13_Greedy.py
+++ 13_Greedy.py
@@ -171,3 +171,39 @@ class Solution:
             level += 1
         
         return level
+    
+
+
+
+# 6. Gas Station
+"""
+https://leetcode.com/problems/gas-station/
+There are n gas stations along a circular route, where the amount of gas at the ith station 
+is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith 
+station to its next (i + 1)th station. You begin the journey with an empty tank at one of the 
+gas stations.
+
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel 
+around the circuit once in the clockwise direction, otherwise return -1.
+
+If there exists a solution, it is guaranteed to be unique
+"""
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # check whether solution exists
+        if sum(gas) < sum(cost):
+            return -1
+        
+        total = 0
+        idx = 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0:
+                total = 0
+                idx = i + 1
+
+        return idx
+        
--- 15_Intervals.py
+++ 15_Intervals.py
@@ -163,3 +163,92 @@ Output: 2
 Input: intervals = [[7,10],[2,4]]
 Output: 1
 """
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        start, end = [], []
+        for s, e in intervals:
+            start.append(s)
+            end.append(e)
+
+        start.sort()
+        end.sort()
+
+        max_rooms, room_so_far = 0, 0
+        s_ptr, e_ptr = 0, 0
+        while s_ptr < len(intervals):
+            if start[s_ptr] < end[e_ptr]:
+                s_ptr += 1
+                room_so_far += 1
+            else:
+                e_ptr += 1
+                room_so_far -= 1
+
+            max_rooms = max(max_rooms, room_so_far)
+
+        return max_rooms
+    
+
+
+
+# 6. Remove Covered Intervals
+"""
+https://leetcode.com/problems/remove-covered-intervals/
+Given an array intervals where intervals[i] = [li, ri] represent the interval [li, ri), 
+remove all intervals that are covered by another interval in the list.
+
+The interval [a, b) is covered by the interval [c, d) if and 
+only if c <= a and b <= d.
+
+Return the number of remaining intervals.
+
+Input: intervals = [[1,4],[3,6],[2,8]]
+Output: 2
+Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+"""
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        # sort by smaller start and larger end point first
+        intervals.sort(key=lambda x: (x[0], -x[1])) 
+
+        res = [intervals[0]]
+        for start, end in intervals[1:]:
+            prevS, prevE = res[-1]
+            if prevS <= start and prevE >= end:
+                continue
+            res.append([start, end])
+        return len(res)
+
+
+
+
+# 6. Minimum Interval to Include Each Query
+"""
+https://leetcode.com/problems/minimum-interval-to-include-each-query/
+You are given a 2D integer array intervals.
+The size of an interval is defined as the number of integers it contains,
+or more formally righti - lefti + 1.
+
+You are also given an integer array queries. The answer to the jth query is the size of 
+the smallest interval i such that lefti <= queries[j] <= righti. If no such interval exists, 
+the answer is -1.
+
+Input: intervals = [[1,4],[2,4],[3,6],[4,4]], queries = [2,3,4,5]
+Output: [3,3,1,4]
+Explanation: The queries are processed as follows:
+- Query = 2: The interval [2,4] is the smallest interval containing 2. The answer is 4 - 2 + 1 = 3.
+- Query = 3: The interval [2,4] is the smallest interval containing 3. The answer is 4 - 2 + 1 = 3.
+- Query = 4: The interval [4,4] is the smallest interval containing 4. The answer is 4 - 4 + 1 = 1.
+- Query = 5: The interval [3,6] is the smallest interval containing 5. The answer is 6 - 3 + 1 = 4.
+
+Input: intervals = [[2,3],[2,5],[1,8],[20,25]], queries = [2,19,5,22]
+Output: [2,-1,4,6]
+Explanation: The queries are processed as follows:
+- Query = 2: The interval [2,3] is the smallest interval containing 2. The answer is 3 - 2 + 1 = 2.
+- Query = 19: None of the intervals contain 19. The answer is -1.
+- Query = 5: The interval [2,5] is the smallest interval containing 5. The answer is 5 - 2 + 1 = 4.
+- Query = 22: The interval [20,25] is the smallest interval containing 22. The answer is 25 - 20 + 1 = 6.
+"""
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        # skipped for now
+        pass
