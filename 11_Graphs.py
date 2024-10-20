4772b21a49c947fc905a0efe583d476b532abac8
Ashwin Chafale <chafale@usc.edu>
2024-10-20 14:30:25 -0700

    ++update

--- 11_Graphs.py
+++ 11_Graphs.py
@@ -572,6 +572,7 @@ class DisjointSet:
 
 # Basic 13 - Strongly Connected Components SCCs - Kosaraju's Algorithm
 """
+we can skip
 1.  Strongly connected components are only valid for directed graphs. ***
 
 2. In a directed graph, a Strongly Connected Component is a subset of vertices where every 
@@ -660,6 +661,43 @@ class Solution:
                     islands += 1
                     dfs(i, j)
         return islands
+
+
+
+# Problem 4 - Find the Town Judge
+"""
+https://leetcode.com/problems/find-the-town-judge/description/
+In a town, there are n people labeled from 1 to n. 
+There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+1. The town judge trusts nobody.
+2. Everybody (except for the town judge) trusts the town judge.
+3. There is exactly one person that satisfies properties 1 and 2.
+
+You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai 
+trusts the person labeled bi. If a trust relationship does not exist in trust array, then such a trust 
+relationship does not exist.
+
+Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+
+Example 1:
+
+Input: n = 2, trust = [[1,2]]
+Output: 2
+
+Example 2:
+
+Input: n = 3, trust = [[1,3],[2,3]]
+Output: 3
+
+Example 3:
+
+Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+"""
+
 
 
 
