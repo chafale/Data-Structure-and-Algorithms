c343ac224d79fc42a0e8ee0adb40326ee5623632
Ashwin Chafale <chafale@usc.edu>
2024-10-20 14:34:13 -0700

    ++update

--- 11_Graphs.py
+++ 11_Graphs.py
@@ -697,7 +697,20 @@ Example 3:
 Input: n = 3, trust = [[1,3],[2,3],[3,1]]
 Output: -1
 """
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        indegree = [0 for i in range(n + 1)]
+        outdegree = [0 for i in range(n + 1)]
 
+        for u, v in trust:
+            outdegree[u] += 1
+            indegree[v] += 1
+        
+        for i in range(1, n+1):
+            if indegree[i] == n - 1 and outdegree[i] == 0:
+                return i
+
+        return -1
 
 
 
