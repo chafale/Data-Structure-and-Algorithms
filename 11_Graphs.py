cc34ff26f34b812d050e8b2dc0e3d08f1c993c04
Ashwin Chafale <chafale@usc.edu>
2024-10-20 14:35:03 -0700

    ++update

--- 11_Graphs.py
+++ 11_Graphs.py
@@ -684,17 +684,14 @@ relationship does not exist.
 Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
 
 Example 1:
-
 Input: n = 2, trust = [[1,2]]
 Output: 2
 
 Example 2:
-
 Input: n = 3, trust = [[1,3],[2,3]]
 Output: 3
 
 Example 3:
-
 Input: n = 3, trust = [[1,3],[2,3],[3,1]]
 Output: -1
 """
