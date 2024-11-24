22272bc00a03db629aea6aca17f7cb8def6b7d96
Ashwin Chafale <chafale@usc.edu>
2024-11-24 04:43:39 -0800

    ++update

--- 9_Binary Tree & BST.py
+++ 9_Binary Tree & BST.py
@@ -1509,6 +1509,18 @@ class Solution:
 # Problem 3 - Lowest Common Ancestor of a Binary Search Tree
 """
 https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+The lowest common ancestor is defined between two nodes p and q as the lowest node in T that 
+has both p and q as descendants (where we allow a node to be a descendant of itself).
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself 
+according to the LCA definition.
 """
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
@@ -1633,7 +1645,7 @@ Output
 """
 # Solution : 
 # Take a stack data structure
-# 1. from root node add all the node (from left subtree) which are extreme left
+# 1. from root node add all the node (from left subtree) which are extreme left to the stack
 # 2. when there is a next operation pop element of the queue and print and also 
 #    look for if the node has right -> then all the node in the right tree which are extreme left and
 #    add them in the stack
