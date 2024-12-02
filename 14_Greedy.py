69fd86ae7886737238463f81ac9fc90d91bc38d9
Ashwin Chafale <chafale@usc.edu>
2024-12-02 04:17:30 -0800

    ++update

--- 14_Greedy.py
+++ 14_Greedy.py
@@ -132,7 +132,34 @@ gas stations.
 Given two integer arrays gas and cost, return the starting gas station's index if you can travel 
 around the circuit once in the clockwise direction, otherwise return -1.
 
-If there exists a solution, it is guaranteed to be unique
+If there exists a solution, it is guaranteed to be unique.
+
+Example 1:
+
+Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+Output: 3
+Explanation:
+Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 4. Your tank = 4 - 1 + 5 = 8
+Travel to station 0. Your tank = 8 - 2 + 1 = 7
+Travel to station 1. Your tank = 7 - 3 + 2 = 6
+Travel to station 2. Your tank = 6 - 4 + 3 = 5
+Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+Therefore, return 3 as the starting index.
+
+Example 2:
+
+Input: gas = [2,3,4], cost = [3,4,3]
+Output: -1
+Explanation:
+You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 0. Your tank = 4 - 3 + 2 = 3
+Travel to station 1. Your tank = 3 - 3 + 3 = 3
+You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+Therefore, you can't travel around the circuit once no matter where you start.
+
+https://www.youtube.com/watch?v=lJwbPZGo05A
 """
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
@@ -175,6 +202,7 @@ The third person goes to city B for a cost of 50.
 The fourth person goes to city B for a cost of 20.
 
 The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
+https://www.youtube.com/watch?v=d-B_gk_gJtQ
 """
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
