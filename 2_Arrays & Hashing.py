59f9cadb75d44e241d47265b4ffbaa0a27e3b212
Ashwin Chafale <chafale@usc.edu>
2024-12-09 11:39:15 -0800

    ++update

--- 2_Arrays & Hashing.py
+++ 2_Arrays & Hashing.py
@@ -954,3 +954,43 @@ class Solution:
                 minIdx = i
 
         return minIdx
+
+
+
+
+# 26. Valid Sudoku
+"""
+https://leetcode.com/problems/valid-sudoku/description/
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+"""
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+        
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+
+                if val == ".":
+                    continue
+
+                if (val in rows[r] or
+                    val in cols[c] or
+                    val in squares[(r//3, c//3)]):
+                    return False
+
+                rows[r].add(val)
+                cols[c].add(val)
+                squares[(r//3, c//3)].add(val)
+
+        return True
